fbaa0464347d40efa4d88854b3529abec0c5ae9b
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2021-06-09 16:10:28 -0300

    nova capa social

--- view/header.php
+++ view/header.php
@@ -5,9 +5,9 @@
     <title><?php print htmlentities($title); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="<?php print SITE_URL; ?>favicon.ico">
-    <!-- facebook -->
+    <!-- facebook min200x200 -->
     <meta property="og:image"
-    content="<?php print SITE_URL; ?>img/logo_white.png" /><!--min200x200 fb-->
+    content="<?php print SITE_URL; ?>img/capa_social_2.png" />
     <meta property="og:title" content="<?php print htmlentities($title); ?>" />
     <meta property="og:description" content="<?php print SITE_NAME; ?>" />
     <meta property="og:url" content="<?php print @$canonicalUrl; ?>"/>
@@ -16,7 +16,7 @@
     <meta name="twitter:description" content="<?php print SITE_NAME; ?>">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:image"
-    content="<?php print SITE_URL; ?>img/logo_white.png">
+    content="<?php print SITE_URL; ?>img/capa_social_2.png">
     <meta name="twitter:site" content="@hackergaucho">
     <meta name="twitter:creator" content="@hackergaucho">
     <meta name="twitter:url" content="<?php print @$canonicalUrl; ?>">

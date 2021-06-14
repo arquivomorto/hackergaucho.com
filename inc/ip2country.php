a3a3d513d07aff749cff7ea7260bfa434174cbc0
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2021-06-14 13:00:37 -0300

    M inc/ip2country.php

--- inc/ip2country.php
+++ inc/ip2country.php
@@ -10,6 +10,7 @@ return function ($ip) {
     $whois = new Whois();
     $r = $whois->Lookup($ip);
     $country=@$r['regrinfo']['owner']['address']['country'];
+    die(var_dump($country));
     if (!$country) {
         $country=@$r['regrinfo']["network"]['country'];
     }

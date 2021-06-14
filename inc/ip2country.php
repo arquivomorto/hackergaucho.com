3f0c18317e550a371b428bbf476ecd1f55dc34a1
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2021-06-14 12:55:55 -0300

    M inc/ip2country.php

--- inc/ip2country.php
+++ inc/ip2country.php
@@ -3,15 +3,15 @@
 //v0.1.0 14jun2021
 
 return function ($ip) {
-    // require_once __DIR__.'/src/whois.main.php';
-    // $whois = new Whois();
-    // $r = $whois->Lookup($ip);
-    // $country=@$r['regrinfo']['owner']['address']['country'];
-    // if (!$country) {
-    //     $country=@$r['regrinfo']["network"]['country'];
-    // }
-    $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
-    $country=@$details->country;//ISO 3166-2
+    require_once __DIR__.'/src/whois.main.php';
+    $whois = new Whois();
+    $r = $whois->Lookup($ip);
+    $country=@$r['regrinfo']['owner']['address']['country'];
+    if (!$country) {
+        $country=@$r['regrinfo']["network"]['country'];
+    }
+    // $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+    // $country=@$details->country;//ISO 3166-2
     if ($country) {
         return $country;
     } else {

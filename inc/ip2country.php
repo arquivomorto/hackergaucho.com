5b1fd985c8a77499cff2689e20ab87be81bb991c
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2021-06-14 13:05:41 -0300

    M inc/ip2country.php

--- inc/ip2country.php
+++ inc/ip2country.php
@@ -3,20 +3,15 @@
 //v0.1.0 14jun2021
 
 return function ($ip) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-    require_once __DIR__.'/src/whois.main.php';
-    $whois = new Whois();
-    $r = $whois->Lookup($ip);
-    $country=@$r['regrinfo']['owner']['address']['country'];
-    print '<pre>';
-    die(var_dump($r));
-    if (!$country) {
-        $country=@$r['regrinfo']["network"]['country'];
-    }
-    // $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
-    // $country=@$details->country;//ISO 3166-2
+    // require_once __DIR__.'/src/whois.main.php';
+    // $whois = new Whois();
+    // $r = $whois->Lookup($ip);
+    // $country=@$r['regrinfo']['owner']['address']['country'];
+    // if (!$country) {
+    //     $country=@$r['regrinfo']["network"]['country'];
+    // }
+    $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+    $country=@$details->country;//ISO 3166-2
     if ($country) {
         return $country;
     } else {

79dcad78ac8986dc3551975e46541e4d45e075b0
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2021-06-15 09:31:24 -0300

    fix do bugs nos ips americanos

--- inc/ip2country.php
+++ inc/ip2country.php
@@ -1,6 +1,7 @@
 <?php
 //Criado por @hackergaucho
 //v0.1.0 14jun2021 com rdap e 127.0.0.1 = US
+//v0.1.1 15jun2021 com fix do bugs nos ips americanos
 
 return function ($ip) {
     if ($ip=='127.0.0.1') {
@@ -11,7 +12,20 @@ return function ($ip) {
         if (strlen($country)==2 and ctype_alpha($country)) {
             return $country;
         } else {
-            null;
+            /*
+            fix do bugs nos ips americanos (precisa do ipinfo.io):
+            US 108.62.187.64
+            US 54.245.173.249
+            US 52.12.64.199
+            US 54.244.190.66
+            */
+            $details=json_decode(file_get_contents("http://ipinfo.io/{$ip}"));
+            $country=@$details->country;//ISO 3166-2
+            if (strlen($country)==2 and ctype_alpha($country)) {
+                return $country;
+            } else {
+                return null;
+            }
         }
     }
 };

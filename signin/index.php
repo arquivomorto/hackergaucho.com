d26bbca7939aa40db12098e057eef09955689cb1
Hacker Gaucho <84669018+hackergaucho@users.noreply.github.com>
2021-06-24 09:21:50 -0300

    signin index

--- signin/index.php
+++ signin/index.php
@@ -1,2 +1,19 @@
 <?php
-require '../signin.php';
+require '../cfg.php';
+$method=$_SERVER["REQUEST_METHOD"];
+if ($method=='POST') {
+    $email=@$_POST['email'];
+    $password=@$_POST['password'];
+    if ($email===ADMIN_EMAIL and $password===ADMIN_PASSWORD) {
+        $token=md5(ADMIN_PASSWORD);
+        setcookie('TOKEN', $token);
+        $url=SITE_URL;
+        header('Location: '.$url);
+    } else {
+        $error[]='Dados incorretos';
+        require VIEW.'error.php';
+    }
+} else {
+    require VIEW.'signin.php';
+}
+require HELPER.'logMail.php';
